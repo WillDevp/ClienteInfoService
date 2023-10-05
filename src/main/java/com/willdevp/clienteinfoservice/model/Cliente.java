eef558563c490f1199ae7eda9d555af60c97436c
WillDev <wilmar.garciava@gmail.com>
2023-10-05 15:13:55 -0500

    feat(model): Se agrega clase Cliente al modelo con anotaciones Lombook

---
+++ src/main/java/com/willdevp/clienteinfoservice/model/Cliente.java
@@ -0,0 +1,18 @@
+package com.willdevp.clienteinfoservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cliente {
+    private String primerNombre;
+    private String segundoNombre;
+    private String primerApellido;
+    private String segundoApellido;
+    private String telefono;
+    private String direccion;
+    private String ciudadResidencia;
+}
